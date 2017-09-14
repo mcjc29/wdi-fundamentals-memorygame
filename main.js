f76bff5cf89989eaef19e8d20714125d6758c790
mcjc29 <31503139+mcjc29@users.noreply.github.com>
2017-09-14 09:49:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [ ];
+
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+	} else {
+	alert("Sorry, try again.");
+	}
+}
+
+var flipCard = function(cardId) {
+	checkForMatch()
+	console.log("User flipped" + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+}
+
+flipCard(0)
+flipCard(2)
+
