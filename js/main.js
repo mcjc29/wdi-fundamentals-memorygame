8c6ac708d48cff96c61db19a10d4a8ac750ee0b1
mcjc29 <31503139+mcjc29@users.noreply.github.com>
2017-09-17 16:23:21 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -31,7 +31,10 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	cardId = this.getAttribute('data-id');
+  	this.setAttribute('src', cards[cardId].cardImage);
+
 	checkForMatch()
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
@@ -40,6 +43,15 @@ var flipCard = function(cardId) {
 	console.log(cards[cardId].suit)
 }
 
-flipCard(0)
-flipCard(2)
 
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+}
+}
+
+createBoard();
